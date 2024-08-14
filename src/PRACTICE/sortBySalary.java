fed6198e563952676c365e073f51051b996e8ccd
vaishvik12 <127079238+vaishvik12@users.noreply.github.com>
2024-08-14 19:08:52 +0530

    Update sortBySalary.java

--- src/PRACTICE/sortBySalary.java
+++ src/PRACTICE/sortBySalary.java
@@ -7,6 +7,7 @@ public class sortBySalary implements Comparator<Employee> {
 	@Override
 	public int compare(Employee o1, Employee o2) {
 		// TODO Auto-generated method stub
+		System.out.println("Sorted by Salary");
 		return (int)(o2.getSalary() - o1.getSalary());
 	}
 
