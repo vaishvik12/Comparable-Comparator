ebc71986f683bdd51c2b528cfb50b91b39baddd6
vaishvik12 <vaishvikjamalta@gmail.com>
2024-08-14 19:11:16 +0530

    Added a message to sortByName class
    Looks better now

--- src/PRACTICE/sortByName.java
+++ src/PRACTICE/sortByName.java
@@ -7,6 +7,7 @@ public class sortByName implements Comparator<Employee>{
 	@Override
 	public int compare(Employee e1, Employee e2) {
 		// TODO Auto-generated method stub
+		System.out.println("Sorted by name");
 		return (int)(e2.getName().compareTo(e1.getName()));
 	}
 
